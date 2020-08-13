e601629559c2cb675a969e815942e100e4a9eced
juangm <juangarciamarinblog@gmail.com>
2020-08-13 17:24:52 +0200

    Initial commit

---
+++ template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Template for CFT challengues for steps
+
+case "$1" in
+  # Description
+  1)
+    echo "Step1"
+    ;;
+  # Description task 2
+  2)
+    echo "Step1"
+    ;;
+  *)
+    echo "Select proper step"
+    ;;
+esac
